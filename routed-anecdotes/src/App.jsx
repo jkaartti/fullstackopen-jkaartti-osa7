c0947451b0d9fb3c53df27879466b82284f5cf5e
Joonas Kaartti <joonas.kaartti@gmail.com>
2023-10-05 10:05:33 +0300

    input fields are no longer passed 'reset' attribute, wich resulted in console warning (7.6)

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -66,9 +66,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
 
   const navigate = useNavigate()
 
@@ -86,9 +86,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
